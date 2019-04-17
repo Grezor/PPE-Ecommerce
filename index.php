179d90fd52f4d17bc0705ab020a884acceee76ac
Grezor <grezor.gamer@gmail.com>
2019-04-17 15:15:16 +0200

    1704

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 								<div class="modal-content">
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal">&times;</button>
-										<h4>Resort <span>Inn</span></h4>
+										<h4>Choptaphoto <span>Inn</span></h4>
 										<img src="images/1.jpg" alt=" " class="img-responsive">
 										<h5>We know what you love</h5>
 										<p>Providing guests unique and enchanting views from their rooms with its exceptional amenities, makes Star Hotel one of bests in its kind.Try our food menu, awesome services and friendly staff while you are here.</p>
